06096d33064947a82d594766d674f6791e30a374
bats <jargalsaihan.batsaihan@gmail.com>
2016-07-26 17:37:01 -0700

    added const and icons

---
+++ common/const.js
@@ -0,0 +1,15 @@
+// Tab ids
+export const HomeTabId    = 'HomeTabId';
+export const SearchTabId  = 'SearchTabId';
+export const ReviewTabId  = 'ReviewTabId';
+export const MapTabId     = 'MapTabId';
+export const ProfileTabId = 'ProfileTabId';
+
+
+export const AboutSceneId    = '0';
+export const LaunchSceneId   = '1';
+export const LoginSceneId    = '2';
+export const RegisterSceneId = '3';
+export const ProductSceneId  = '4';
+export const ProducerSceneId = '5';
+export const HomeSceneId     = '6';
