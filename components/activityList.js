8aa2b40c4ba12f32fd1aad4c419e3d00bb77be41
bats <jargalsaihan.batsaihan@gmail.com>
2016-07-29 17:21:13 -0700

    added some files

---
+++ components/activityList.js
@@ -0,0 +1,30 @@
+//
+// Description: activitysection.js
+// This contains the declaration for the activity section
+//
+
+// Import modules
+import React, { Component } from 'react';
+import {StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import ActivityItem from './activityitem.js';
+
+class ActivitySection extends Component {
+    render() {
+        //
+        // BatsFix. This needs to be flexible with regard to a device with and height.
+        //
+        return (
+            <View>              
+                {/* Activity Items BatsFix. should be dynamically generated here*/}
+                <View style={{flexDirection:'row',flexWrap:'wrap'}}>
+                    <ActivityItem activity="music"    imageIndex={0} />
+                    <ActivityItem activity="movie"    imageIndex={1} />
+                    <ActivityItem activity="exercise" imageIndex={2} />
+                    <ActivityItem activity="create"   imageIndex={3} />
+                </View>
+            </View>
+        );
+    }
+}
+
+module.exports = ActivitySection;
