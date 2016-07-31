deada8565f065021b6171de2e6c366c0d4329321
Derek Alia <derekalia@hotmail.com>
2016-07-30 19:05:00 -0700

    styled the review navbar

--- components/reviewTab.js
+++ components/reviewTab.js
@@ -14,9 +14,11 @@ class ReviewTab extends Component {
     render() {
         return (
           <View style={{flex:1}}>
-            <View style={{height:70,alignItems:'center',justifyContent: "center",backgroundColor:'#F9F9F9',borderBottomWidth:1,borderColor:'#B2B2B2'}}>
-              <View style={{marginTop:18}}>
-                <Text style={{fontSize: 18, marginTop: 1, fontWeight: 'bold'}}>Review</Text>
+            <View style={{height:65,alignItems:'center',justifyContent: "center",backgroundColor:'#F9F9F9',borderBottomWidth:1,borderColor:'#B2B2B2'}}>
+              <View style={{flex: 1,flexDirection: 'row',justifyContent: 'space-between', marginTop:30}}>
+                <Text style={{fontSize: 18, width:110, textAlign:"left"}}></Text>
+                <Text style={{fontSize: 18, width:110, textAlign:"center", fontWeight:'bold'}}>Review</Text>
+                <Text style={{fontSize: 18, width:110, textAlign:"right",color:'#4A90E2'}}> Queue</Text>
               </View>
             </View>
             <View style={{alignItems:'center', marginTop:30}}>
@@ -33,7 +35,7 @@ class ReviewTab extends Component {
               </View>
               <View style={{marginTop:20}}>
                 <TouchableHighlight underlayColor='#dddddd'>
-                    <View style={{ backgroundColor: '#007AFF',width:200,height:55,alignItems:'center',justifyContent: "center",borderRadius:15 }}>
+                    <View style={{ backgroundColor: '#4A90E2',width:200,height:55,alignItems:'center',justifyContent: "center",borderRadius:15 }}>
                  <Text style={{color:"white",fontSize:16,textAlign:'center'}}>Submit</Text>
                  </View>
                  </TouchableHighlight>
