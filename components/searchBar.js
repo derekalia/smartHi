19baff8aa16826a84bf6e9014bbaed20c926a4a2
Derek Alia <derekalia@hotmail.com>
2016-08-07 13:17:46 -0700

    put more curves on the searchbar

--- components/searchBar.js
+++ components/searchBar.js
@@ -16,16 +16,18 @@ class SearchBar extends Component {
         return (
              <View style={{flexDirection: "row", marginTop: 20, marginBottom: 0, marginHorizontal: 10,}}>
                 <View style={[{ flex: 5,}]}>
+                <View style={{height: 50,borderWidth:1,borderColor:'#4A90E2',borderTopLeftRadius:6,borderBottomLeftRadius:6,}}>
                     <TextInput style={[Styles.input]}
                         autoCapitalize  = "none"
                         autoCorrect     = {false}
-                        placeholder     = " Keyword"
+                        placeholder     = "Keyword"
                         returnKeyType   = "next"
                         onChange        = {(e) => this._onChange(e)}
                         clearButtonMode = 'always'
                         />
+                        </View>
                 </View>
-                <View style={[{ flex: 1, borderWidth: 1, borderColor: "#4A90E2", backgroundColor: "#4A90E2", height: 50,borderTopRightRadius:0,borderBottomRightRadius:0 }]}>
+                <View style={[{ flex: 1, borderWidth: 1, borderColor: "#4A90E2", backgroundColor: "#4A90E2", height: 50,borderTopRightRadius:6,borderBottomRightRadius:6 }]}>
                     <TouchableOpacity style={{}} onPress={this.props.startSearch}>
                         <Image style={{ height: 32, width: 32, marginLeft: 13, marginTop: 8 }} source={require("../media/SearchIcon0.png") }/>
                     </TouchableOpacity>
@@ -39,15 +41,9 @@ module.exports = SearchBar;
 
 const Styles={
     input: {
-        height: 50,
+        height: 45,
+        marginLeft:7,
         fontSize: 20,
-        borderWidth: 1.5,
-        borderTopLeftRadius:10,
-        borderBottomLeftRadius:10,
         backgroundColor: 'white',
-
-        borderColor: "#4A90E2",
-
-
     },
 }
