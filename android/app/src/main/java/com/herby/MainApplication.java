dc4b64b74cd024446e2856a736ee3639c2c6ac6c
Farzad E <dMe@dNetGuru.com>
2016-08-08 23:00:11 -0700

    Compiles for Android but crashes!

--- android/app/src/main/java/com/herby/MainApplication.java
+++ android/app/src/main/java/com/herby/MainApplication.java
@@ -4,11 +4,12 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.lwansbrough.ReactCamera.ReactCamera;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+
+import com.lwansbrough.RCTCamera.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactCamera()
+	  new RCTCameraPackage()
       );
     }
   };
