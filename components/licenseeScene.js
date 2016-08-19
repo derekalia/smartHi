2e96e896f657211a6385f7749b644486ef5e6739
bats <jargalsaihan.batsaihan@gmail.com>
2016-08-19 14:37:23 -0700

    move to use common controls for licensee and settings etc

--- components/licenseeScene.js
+++ components/licenseeScene.js
@@ -3,75 +3,43 @@
 // Used for testing various UI components
 //
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, Slider, ListView, ListViewDataSource, ScrollView, Image, TextInput, TouchableHighlight, Navigator} from 'react-native'
+//get state management components
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+
+import {StyleSheet, Text, View, } from 'react-native'
+
+import {SwitchSceneAction,} from '../actions';
+import {LicenseeStoreSceneId,} from '../common/const.js';
+
+import {HerbyButton,HerbyHeader,HerbyInput,HerbyAlert} from '../common/controls.js';
+
 
 class LicenseeScene extends Component {
     constructor(props) {
         super(props);
     }
-
+    _onDone() {
+        //BatsFix. for now just switch to upload store scene        
+        this.props.SwitchSceneAction(LicenseeStoreSceneId);
+    }
     render() {
         return (
-          <View>
-            <View style={{backgroundColor:'#EFEFF4',height:600, marginTop:63}}>
-              <View style={{marginTop:30}}>
-                <Text style={{marginLeft:20,marginBottom:10}}>USER INFO</Text>
-                <View style={{backgroundColor:'white',borderTopWidth:1,borderTopColor:'#C8C8CC',borderBottomWidth:1,borderBottomColor:'#C8C8CC'}}>
-                  <View style={{marginLeft:20,marginTop:10,marginBottom:10,flexDirection:'row',height:26,alignItems:'center'}}>
-                    <TouchableHighlight><Text>State</Text></TouchableHighlight>
-                    <View style={{alignItems:'flex-end',flex:1,marginRight:20}}>
-                    <Text style={{color:'#C8C8CC'}}>Washington</Text>
-                    </View>
-                  </View>
-
-                  <View style={{alignSelf:'flex-end',width:1,width:356,borderTopWidth:1,borderTopColor:'#C8C8CC'}}></View>
-
-                  <View style={{marginLeft:20,marginTop:10,marginBottom:10,flexDirection:'row',height:26,alignItems:'center'}}>
-                    <TouchableHighlight><Text>Email</Text></TouchableHighlight>
-                    <View style={{alignItems:'flex-end',flex:1,marginRight:20}}>
-                      <Text style={{color:'#C8C8CC'}}>derek@forged.io</Text>
-                    </View>
-                  </View>
-
-                  <View style={{alignSelf:'flex-end',width:1,width:356,borderTopWidth:1,borderTopColor:'#C8C8CC'}}></View>
-
-                  <View style={{marginLeft:20,marginTop:10,marginBottom:10,flexDirection:'row',height:26,alignItems:'center'}}>
-                    <TouchableHighlight><Text>UBI</Text></TouchableHighlight>
-                    <View style={{alignItems:'flex-end',flex:1,marginRight:20}}>
-                      <Text style={{color:'#C8C8CC'}}>481939</Text>
-                    </View>
-                  </View>
-                  <View style={{alignSelf:'flex-end',width:1,width:356,borderTopWidth:1,borderTopColor:'#C8C8CC'}}></View>
-
-                  <View style={{marginLeft:20,marginTop:10,marginBottom:10,flexDirection:'row',height:26,alignItems:'center'}}>
-                    <TouchableHighlight><Text>Password</Text></TouchableHighlight>
-                    <View style={{alignItems:'flex-end',flex:1,marginRight:20}}>
-                      <Text style={{color:'#C8C8CC'}}>*******</Text>
-                    </View>
-                  </View>
-                </View>
-              </View>
-            </View>
-
-
-
-                <View style={{marginTop:200}}>
-                  <Text style={{marginLeft:20,marginBottom:10}}>Status</Text>
-                  <View style={{backgroundColor:'white',borderTopWidth:1,borderTopColor:'#C8C8CC',borderBottomWidth:1,borderBottomColor:'#C8C8CC'}}>
-                    <View style={{marginLeft:20,marginTop:10,marginBottom:10,flexDirection:'row',height:26,alignItems:'center'}}>
-                      <TouchableHighlight><Text>Licensee Login</Text></TouchableHighlight>
-                      <View style={{alignItems:'flex-end',flex:1,marginRight:20}}>
-                        <Image source={require('../media/ForwardArrow2.png') } style={{ width: 8, height: 14,alignItems:'flex-end' }}/>
-                      </View>
-                    </View>
-                  </View>
-                </View>
-
-
-
+            <View style={{backgroundColor:'#EFEFF4',flex:1,}}>
+                <HerbyHeader name="User Info"/>
+                <HerbyInput  name="State"    value="Type state here"/>
+                <HerbyInput  name="Email"    value="Type email here"/>
+                <HerbyInput  name="UBI"      value="Type UBI here"/>
+                <HerbyInput  name="Password" value="Type password here"/>
+                <HerbyButton name="Done" onPress={()=> this._onDone()}/>
             </View>
         );
     }
 }
 
-module.exports = LicenseeScene;
+//
+// Connect SwitchSceneAction to props
+//
+function mapActionToProps(dispatch) { return bindActionCreators({ SwitchSceneAction }, dispatch); }
+module.exports = connect(null,mapActionToProps)(LicenseeScene);
--- components/settingsScene.js
+++ components/settingsScene.js
@@ -10,102 +10,7 @@ import {connect} from 'react-redux';
 
 import {SwitchSceneAction, GetProductAction} from '../actions';
 import {LicenseeSceneId,} from '../common/const.js';
-import HerbyButton from './herbyButton.js';
-
-class HerbyHeader extends Component {
-    render() {
-        return(
-        <Text style={{marginLeft:20,paddingTop:10,paddingBottom:10,}}>{this.props.name}</Text>
-        );
-    }
-}
-class HerbyInput extends Component {
-    _onChange(e) {
-    }
-    _renderName() {
-        if (this.props.name != null) {
-            return (
-            <Text style={{width:100}}>{this.props.name}</Text>
-            );
-        }
-        return null;
-    }
-
-    render() {
-        return(
-        <View style={{
-            height:36,
-            paddingLeft:20,
-            paddingTop:10,
-            paddingBottom:10,
-            marginLeft:0,
-            flexDirection:'row',
-            alignItems:'stretch',
-            backgroundColor:'white',
-            borderBottomWidth:1,
-            borderBottomColor:'#C8C8CC'}}>
-            {this._renderName()}
-          <TextInput style={{width:320,alignItems:'stretch'}} placeholder = {this.props.value}/> 
-        </View>
-        );
-    }
-}
-class HerbyAlert extends Component {
-    constructor(props) {
-        super(props);
-        var {width,height} = Dimensions.get('window');
-        this._width    = width;
-        this._height   = this.props.height;
-        // starts above screen
-        this._topStart = -this.props.height;
-        // ends up half way of the screen
-        this._topEnd   = (height-this.props.height)/2;
-        this._child    = this.props.children;
-        this.state = {
-            anim: new Animated.Value(0),
-            show:this.props.show
-        };
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.show) {
-            this._startSequence();
-        }
-        else {
-            this._dismiss();
-        }
-    }
-
-     _startSequence() {
-        Animated.sequence([
-            Animated.timing(this.state.anim,{toValue:200,duration:1600}),
-            Animated.spring(this.state.anim,{toValue:100,duration:1600}),
-        ]).start();
-    }
-
-     _dismiss() {
-        Animated.timing(this.state.anim,{toValue:0,duration:1000}).start();
-    }
-  
-    render() {
-        const styleSlideDown = {
-            transform: [
-                {translateY: this.state.anim.interpolate({
-                    inputRange: [0, 100],
-                    outputRange: [this._topStart, this._topEnd],
-                })},
-            ],
-            width:this._width,
-            height:this._height,
-        };
-        //BatsFix. Notification elements should be included below 
-        return (
-            <Animated.View style={[{left:0,top:this._topStart,position:'absolute',backgroundColor:'white',borderRadius:20,borderWidth:1,},styleSlideDown]}>
-                {this._child}
-            </Animated.View>
-        );
-    }
-}
+import {HerbyButton,HerbyHeader,HerbyInput,HerbyAlert} from '../common/controls.js';
 
 class SettingsScene extends Component {
     constructor(props) {
@@ -126,11 +31,11 @@ class SettingsScene extends Component {
 
     render() {
         return (
-            <View>
+            <View style={{flex:1}}>
                 {/*Popup settings area*/}
              
                 {/*Main settings area*/}
-                <View style={{backgroundColor:'#C8C8CC',flex:1,}}>
+                <View style={{backgroundColor:'#EFEFF4',flex:1,}}>
                 <HerbyHeader name="GENERAL"/>
                 <HerbyInput  name="UserName" value={this._userName}/>
                 <HerbyInput  name="Email" value={this._email}/>
@@ -138,10 +43,10 @@ class SettingsScene extends Component {
                 <HerbyHeader name="AUTHENTICATION"/>
                 <HerbyButton name="Licensee Login" onPress={()=> this._goLicensee()}/>
                 </View>
-                 <HerbyAlert show={this.state.showAlert} height={120}>
+                 <HerbyAlert show={this.state.showAlert} height={136}>
                     <View style={{backgroundColor:'white',marginLeft:20,marginRight:20,}}>
-                    <HerbyInput  value="password"/>
-                    <HerbyInput  value="password"/>
+                    <HerbyInput  value="Type password"/>
+                    <HerbyInput  value="Type password again"/>
                     <HerbyButton name="Done" onPress={()=>this._goResetPassword()}/>
                     </View>
                 </HerbyAlert>
