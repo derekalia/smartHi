2109c7cfc10cea594c9c9180dd2d34675ef573d8
bats <jargalsaihan.batsaihan@gmail.com>
2016-09-15 15:38:03 -0700

    add filtering button to retailerFrame

--- components/retailerFrame.js
+++ components/retailerFrame.js
@@ -3,7 +3,7 @@
 // Used for searching and listing products 
 //
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, Slider, ListView, ListViewDataSource, ScrollView, Image, TextInput, TouchableHighlight, Navigator} from 'react-native'
+import {StyleSheet, Text, View, Slider, ListView, ListViewDataSource, ScrollView, Image, TextInput, TouchableOpacity, Navigator} from 'react-native'
 //get state management components
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
@@ -16,6 +16,7 @@ import {HerbyMulti} from '../common/controls.js';
 class RetailerFrame extends Component {
     constructor(props) {
         super(props);
+        this.state = {showFilters:true};
     }
 
     _goRetailer(retailerId) {
@@ -24,11 +25,13 @@ class RetailerFrame extends Component {
         //
        this.props.GetRetailerAction(retailerId);
     }
-
-
-    render() {
-        return(
-        <View style={{backgroundColor:'white',flex:1,flex:1}}>
+    _switchFiltering() {
+        this.setState({showFilters:!this.state.showFilters});
+    }
+    _renderFilters() {
+        if (this.state.showFilters) {
+            return (
+            <View>
             <HerbyMulti items={['Any','$','$$','$$$','$$$$']} label='Price'/>
             <View style={{flex:1,margin:5,borderBottomColor:'#DEDEDE',borderBottomWidth:2}}/>
             <HerbyMulti items={['Any','0.5 mi','1 mi','5 mi','10 mi']} label='Distance'/>
@@ -36,6 +39,22 @@ class RetailerFrame extends Component {
             <HerbyMulti items={['Any','1 star','2 stars','3 stars','4 stars']} label='Rating at least'/>
             <View style={{flex:1,margin:5,borderBottomColor:'#DEDEDE',borderBottomWidth:2}}/>
             <HerbyMulti items={['Any','Open Now']} label='Hours'/>
+            </View>
+            );
+        }
+        return null;
+    }
+    render() {
+        return(
+        <View style={{backgroundColor:'white',flex:1,flex:1}}>
+            <TouchableOpacity onPress={()=> this._switchFiltering()}>
+                <View style={{height:40,margin:20,backgroundColor:'white',borderColor: '#9b9b9b',borderWidth:1, borderRadius:22,justifyContent:'center'}}>
+                    <Text style={{fontSize:14,color:'#9b9b9b',alignSelf:'center'}}>
+                        {this.state.showFilters?'Hide Filters':'Show Filters'}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+            {this._renderFilters()}
             {/*Search results section*/}
             <RetailerList retailers={this.props.retailerList} goRetailer={(id)=> this._goRetailer(id)}/>
         </View>
--- components/userFrame.js
+++ components/userFrame.js
@@ -5,6 +5,36 @@
 import React, { Component } from 'react';
 import {StyleSheet, Text, View, Slider, ListView, ListViewDataSource, ScrollView, Image, TextInput, TouchableHighlight, Navigator} from 'react-native'
 
+class UserFrameItem extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+        <View style={{flexDirection:'row', marginTop:10,marginHorizontal:20}}>
+          <View style={{flex:.25}}>
+            <Image style={{height:60,width:60}} source={require('../media/headshot1.png') }/>
+          </View>
+          <View style={{flex:.8}}>
+              <View style={{flexDirection:'row'}}>
+                  <View style={{marginRight:8}}>
+                    <Text style={{fontSize:18,fontWeight:'bold'}}>{this.props.user.name}</Text>
+                  </View>
+                  <View style={{flexDirection:'row',justifyContent:'flex-end',alignItems:'center'}}>
+                    <Image style={{height:20,width:20}} source={require('../media/Oval129.png') }/>
+                    <Text style={{marginLeft:8}}>{this.props.user.score}</Text>
+                  </View>
+              </View>
+          </View>
+          <View style={{flex:.15,justifyContent:'center'}}>
+          <TouchableHighlight>
+              <Image style={{height:31-2,width:34-2,alignItems:'flex-end',alignSelf:'center'}} source={require('../media/emptyHeart11.png') }/>
+            </TouchableHighlight>
+          </View>
+        </View>
+        );
+    }
+}
 class UserFrame extends Component {
     constructor(props) {
         super(props);
@@ -13,33 +43,7 @@ class UserFrame extends Component {
     render() {
         return(
         <ScrollView style={{marginTop: 0,}}>
-
-            <View style={{flexDirection:'row', marginTop:10,marginHorizontal:20}}>
-              <View style={{flex:.25}}>
-                <Image style={{height:60,width:60}} source={require('../media/headshot1.png') }/>
-              </View>
-              <View style={{flex:.8}}>
-                <View style={{flexDirection:'column'}}>
-                  <View style={{flexDirection:'row'}}>
-                      <View style={{marginRight:8}}>
-                        <Text style={{fontSize:18,fontWeight:'bold'}}>TommyChong</Text>
-                      </View>
-                      <View style={{flexDirection:'row',justifyContent:'flex-end',alignItems:'center'}}>
-                        <Image style={{height:20,width:20}} source={require('../media/Oval129.png') }/>
-                        <Text style={{marginLeft:8}}>123</Text>
-                      </View>
-                  </View>
-                  <View style={{flexDirection:'row'}}>
-                  </View>
-                </View>
-              </View>
-              <View style={{flex:.15,justifyContent:'center'}}>
-              <TouchableHighlight>
-                  <Image style={{height:31-2,width:34-2,alignItems:'flex-end',alignSelf:'center'}} source={require('../media/emptyHeart11.png') }/>
-                </TouchableHighlight>
-              </View>
-            </View>
-
+            <UserFrameItem user={{name:'Tommy Chong',score:23}}/>
         </ScrollView>
         );
     }
