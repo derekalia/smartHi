db9c83a62da6c18df9834efc5b7e4cdcb2e25aba
bats <jargalsaihan.batsaihan@gmail.com>
2016-10-09 16:29:13 -0700

    update retailer info update scene. not sure abt how to set the location though

--- components/scenes/updateLicensee/updateLicenseeScene.js
+++ components/scenes/updateLicensee/updateLicenseeScene.js
@@ -4,7 +4,7 @@
 
 // Import modules
 import React, { Component } from 'react';
-import {Alert,TextInput, Modal,Dimensions,StyleSheet, View, Text, ScrollView, Image, Navigator, TouchableOpacity, Platform } from 'react-native';
+import {MapView, Alert,TextInput, Modal,Dimensions,StyleSheet, View, Text, ScrollView, Image, Navigator, TouchableOpacity, Platform } from 'react-native';
 
 import ImagePicker from 'react-native-image-picker';
 
@@ -32,6 +32,9 @@ class UpdateLicenseeScene extends Component {
         }
         var result = this.props.UpdateRetailerAction(this.id,this._name,this._description, sourceImage);
         Alert.alert("",result);
+    }
+    _setLocation() {
+        //BatsFix. Not sure what to do here
     }
     _setDescription(t) {
         this._description = t;
@@ -72,22 +75,28 @@ class UpdateLicenseeScene extends Component {
     render() {
         return(
         <View style={{flex:1}}>
-            <HerbyBar navigator={this.props.navigator} name="Update Store" back="Back" forward="Done" forwardCallback={()=>this._updateLicensee()}/>
-            <View style={{backgroundColor:'white',marginTop:0,margin:10,}}>
+            <HerbyBar navigator={this.props.navigator} name="Update Store" back="Back" forward="Done" forwardCallback={()=>this._updateRetailer()}/>
+            <ScrollView style={{backgroundColor:'white',marginTop:0,marginBottom:50,marginLeft:10,marginRight:10,flex:1,}}>
                 <Image source={this.state.imageSource} style={{ height: 190, width: 380,justifyContent:'center',alignItems:'center'}}/>
-                <View style={{flexDirection:'row',marginTop:20,}}>
+                <HerbyButton2 name="Pick Image" onPress={()=>this._showImagePicker()}/>
+                <View style={{flexDirection:'row',marginTop:5,}}>
                     <Text>Title:</Text>
                     <TextInput style={{flex:1,backgroundColor:'#EDEDED'}} placeholder={this._name} onChangeText={(t)=>this._setName(t)}/>
                 </View>
-                <View style={{marginTop:20}}>
+                <View style={{marginTop:5,}}>
                     <Text>Description:</Text>
-                    <TextInput style={{flex:1,height:200,backgroundColor:'#DEDEDE'}} placeholder={this._description} onChangeText={(t)=>this._setDescription(t)} multiline={true}/>
+                    <TextInput style={{flex:1,height:50,backgroundColor:'#DEDEDE'}} placeholder={this._description} onChangeText={(t)=>this._setDescription(t)} multiline={true}/>
                 </View>
-                <View style={{flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
-                    <HerbyButton2 name="Pick Image" onPress={()=>this._showImagePicker()}/>
-                    <HerbyButton2 name="Update" onPress={()=>this._updateRetailer()}/>
+                <View style={{marginTop:5,flex:1}}>
+                <MapView 
+                    style={{height:200,width:400}}
+                    showsUserLocation={true}
+                    region={{ latitude: 47.597713, longitude: -122.321777, latitudeDelta: 0.5, longitudeDelta: 0.5, }}
+                    showsCompass = {true}
+                    />
+                <HerbyButton2 name="Set Location" onPress={()=>this._setLocation()}/>
                 </View>
-            </View>
+            </ScrollView>
         </View>
         );
     }
