50583562cbf18024f25e123ba4ab95998a6ee286
Derek Alia <derekalia@me.com>
2016-11-14 10:59:43 -0800

    more firebase analytics

--- ios/herby/AppDelegate.m
+++ ios/herby/AppDelegate.m
@@ -11,11 +11,13 @@
 
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
+@import Firebase;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [FIRApp configure];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
